ca730d27f54b4e9b11ebe693302dbb5076120754
Yudong An <ayd1@qq.com>
2021-05-14 11:29:49 +0800

    change minSdkVersion to 23 add activity_map_poi_search.xml, MapPoiSearch.java

---
+++ app/src/main/java/com/greyka/imgr/activities/MapPoiSearch.java
@@ -0,0 +1,16 @@
+package com.greyka.imgr.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.greyka.imgr.R;
+
+public class MapPoiSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_poi_search);
+    }
+}
